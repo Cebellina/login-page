2c31b730b69f1cb5e5e5213895534cca8a48f8b3
Emma Andersson <emma.andersson.se@outlook.com>
2026-02-13 18:34:00 +0100

    Added txt

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const root = document.getElementById("root");
+
+const header = document.createElement("header");
+
+const logo = document.createElement("h1");
+logo.innerText = "img.png";
+logo.classList.add("img");
+
+
+
+header.appendChild(logo);
+root.appendChild(header);
