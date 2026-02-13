fda53fc73d4b67b92d00a7fcd384f80bed4b410e
Emma Andersson <emma.andersson.se@outlook.com>
2026-02-13 23:04:43 +0100

    Deleted 'storage.html' and 'storage.js'

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Skapa/spara användare 
+const user = {
+    name: "test",
+    password: "1234"
+};
+
+localStorage.setItem("user", JSON.stringify(user));
+
+
+// Hämta användare i localStorage 
+const storedUser = JSON.parse(localStorage.getItem("user"));
+
+console.log("Användarnamn:", storedUser.name);
+console.log("Lösenord:", storedUser.password);
+
+
+// Uppdatera lösenord i localStorage 
+function updatePassword(newPassword) {
+const user = JSON.parse(localStorage.getItem("user"));
+
+if (!user) return;
+user.password = newPassword;
+localStorage.setItem("user", JSON.stringify(user));
+}
+
+
+// Testa uppdatering 
+updatePassword("5678");
+
+const updatedUser = JSON.parse(localStorage.getItem("user"));
+console.log("Uppdaterat lösenord:", updatedUser.password);
+
+
 // Sparar användardata i localStorage
 if (!localStorage.getItem("users")) {
     localStorage.setItem("users", JSON.stringify([]));
